47c9d851b52ae1c25899d4b67f7280e8087b9221
Matheus Vieira <matheus.v4200@gmail.com>
2021-11-06 00:42:10 -0300

    tcp socket envelopado em uma classe

--- src/socket/socket.cpp
+++ src/socket/socket.cpp
@@ -1,16 +1,70 @@
-#pragma once
-#include <sys/socket.h>
+#include "./socket.hpp"
+#include <iostream>
 
+Socket::Socket(std::string address, unsigned short port) : address{address}
+{
+	fileDescriptor = socket(PF_INET, SOCK_STREAM, getprotobyname("tcp")->p_proto);
 
-class Socket{
+	if (fileDescriptor == -1)
+	{
+		throw std::runtime_error("Não foi possível criar o socket");
+	}
 
-private:
-	int fileDescriptor;
-	void acceptConnection();
+	std::memset(&socketAddr, 0, sizeof(socketAddr));
 
-public:
-	Socket();
-	void bind();
-	void listen();
-	void connect();
-};
+	socketAddr.sin_family = AF_INET;
+	socketAddr.sin_addr.s_addr = (this->address == "") ? htonl(INADDR_ANY) : inet_addr(this->address.c_str());
+	socketAddr.sin_port = htons(port);
+	isClosed = false;
+}
+
+Socket::~Socket()
+{
+	closes();
+}
+
+void Socket::makeBind()
+{
+	int set_true = 1;
+
+	if (bind(fileDescriptor, (struct sockaddr *)&socketAddr, sizeof(socketAddr)) == 0)
+		return;
+	if (setsockopt(fileDescriptor, SOL_SOCKET, SO_REUSEADDR, &set_true, sizeof set_true) != -1)
+		return;
+
+	throw std::runtime_error("Não foi possível realizar o bind");
+}
+
+void Socket::startListen(int maxNumberOfConnections)
+{
+	if (listen(fileDescriptor, maxNumberOfConnections) == -1)
+	{
+		throw std::runtime_error("Erro ao iniciar a escuta");
+	}
+}
+
+bool Socket::acceptConnection(int &connectionFileDescriptor)
+{
+	struct sockaddr_storage their_addr;
+	socklen_t addr_size = sizeof their_addr;
+	connectionFileDescriptor = accept(fileDescriptor, (struct sockaddr *)&their_addr, &addr_size);
+	return connectionFileDescriptor == -1 ? false : true;
+}
+
+void Socket::makeConnection()
+{
+	if (connect(fileDescriptor, (struct sockaddr *)&socketAddr, sizeof(socketAddr)) == -1)
+	{
+		throw std::runtime_error("Não foi possível se conectar");
+	};
+}
+
+int Socket::getFileDescriptor(){
+	return fileDescriptor;
+}
+
+void Socket::closes()
+{
+	if(!isClosed) close(fileDescriptor);
+	isClosed = true;
+}
