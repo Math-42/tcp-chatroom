c86877bfb66ef22a7c7535b75d70193faf0824ab
Matheus Vieira <matheus.v4200@gmail.com>
2021-11-07 03:31:38 -0300

    adicionand gui no client

--- src/client.cpp
+++ src/client.cpp
@@ -1,7 +1,10 @@
-#include "client/client.hpp"
+#include "communicator/communicator.hpp"
+#include "gui/window.hpp"
 
 int main() {
-    Client client(3090, 80, "127.0.0.1");
-    client.run();
+    Communicator* communicator = Communicator::getInstance(3050, 80, "127.0.0.1");
+    Window clientWindow(50, 50, 1);
+    clientWindow.show();
+    communicator->run();
     return 0;
 }
