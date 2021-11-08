90e3965065c722a915317b5043529c4c66f3dc2c
Matheus Vieira <matheus.v4200@gmail.com>
2021-11-08 16:28:33 -0300

    documentação global controler

--- src/utils/global-controler/global-controler.cpp
+++ src/utils/global-controler/global-controler.cpp
@@ -1,5 +1,5 @@
 #include "./global-controler.hpp"
-
+//inicializa variável estática que contem todas as mensagens mostradas pelo cliente
 std::vector<std::string> GlobalControler::messageHistory = std::vector<std::string>{};
 
 void GlobalControler::addNewMessage(std::string newMessage) {
--- src/utils/global-controler/global-controler.hpp
+++ src/utils/global-controler/global-controler.hpp
@@ -9,5 +9,9 @@ class GlobalControler {
     static std::vector<std::string> messageHistory;
 
    public:
+    /**
+	 * Adiciona uma nova mensagem no vetor de mensagens
+	 * @param newMessage mensagem que será adicionada
+	 */
     static void addNewMessage(std::string newMessage);
 };
