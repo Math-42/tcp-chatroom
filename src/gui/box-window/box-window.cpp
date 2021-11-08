f073cea672f85a0f890363a4667a427181daec11
Matheus Vieira <matheus.v4200@gmail.com>
2021-11-08 16:40:53 -0300

    documentação do box window

--- src/gui/box-window/box-window.cpp
+++ src/gui/box-window/box-window.cpp
@@ -1,15 +1,10 @@
 #include "./box-window.hpp"
 
 #include <iostream>
-BoxWindow::BoxWindow(int height, int width, int startHeight, int startWidth) : title{""} {
-    container = newwin(height, width, startHeight, startWidth);
-    window = newwin(height - 2, width - 2, startHeight + 1, startWidth + 1);
-    setup();
-}
 
 BoxWindow::BoxWindow(std::string title, int height, int width, int startHeight, int startWidth) : title{title} {
-    container = newwin(height, width, startHeight, startWidth);
-    window = newwin(height - 2, width - 2, startHeight + 1, startWidth + 1);
+    container = newwin(height, width, startHeight, startWidth);               //cria um container que possui a caixa
+    window = newwin(height - 2, width - 2, startHeight + 1, startWidth + 1);  //cria a janela real
     setup();
 }
 
@@ -25,6 +20,6 @@ void BoxWindow::refresh() {
 }
 
 void BoxWindow::setup() {
-    box(container, ACS_VLINE, ACS_HLINE);
+    box(container, ACS_VLINE, ACS_HLINE);  //desenha a caixa em volta da janela criada
     if (title != "") mvwprintw(container, 0, 2, ("|" + title + "|").c_str());
 }
--- src/gui/box-window/box-window.hpp
+++ src/gui/box-window/box-window.hpp
@@ -13,12 +13,34 @@ class BoxWindow {
     WINDOW* window;
 
    protected:
+    /**
+	 * Faz o setup inicial da janela criando a caixa de caracteres com o título
+	 */
     void setup();
 
    public:
-    BoxWindow(int height, int width, int startHeight, int startWidth);
+    /**
+	 * Construtor da janela
+	 * @param title titulo da janela
+	 * @param height altura da janela
+	 * @param width largura da janela
+	 * @param startHeigh posição X onde a janela será desenhada
+	 * @param startWidth posição Y onde a janela será desenhada
+	 */
     BoxWindow(std::string title, int height, int width, int startHeight, int startWidth);
+
+    /**
+	 * Destrutor da janela
+	 */
     ~BoxWindow();
+
+    /**
+	 * Função virtual que deve ser sobrescrita para indicar como a janela deve ser desenhada
+	 */
     virtual void draw() = 0;
+
+    /**
+	 * Faz o refresh da janela atualizando de acordo com o que está escrito no buffer
+	 */
     void refresh();
 };
