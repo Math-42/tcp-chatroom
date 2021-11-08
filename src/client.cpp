03a5259a0acc86862aac467eb4a0721232552157
Matheus Vieira <matheus.v4200@gmail.com>
2021-11-08 17:09:23 -0300

    documentação dos executáveis

--- src/client.cpp
+++ src/client.cpp
@@ -6,20 +6,26 @@
 Window* clientWindow;
 Communicator* communicator;
 
+// função para encerrar o programa
 void kill(int) {
     communicator->disconnect();
     delete communicator;
     delete clientWindow;
+    exit(0);
 }
 
+// função mock
 void nothing(int) {}
 
 int main(int argc, char* argv[]) {
+    //usa o nome anom caso um nome não tenha sido passado
     std::string userName = (argc >= 2) ? argv[1] : "anom";
 
     communicator = Communicator::getInstance(4040, 256, "127.0.0.1", userName);
 
-    clientWindow = new Window(50, 50, 1);
+    clientWindow = new Window(1);
+
+    // cadastra os sinais para garantir que o programa finalize corretamente
     std::signal(SIGWINCH, nothing);
     std::signal(SIGINT, kill);
     std::signal(SIGTERM, kill);
--- src/server.cpp
+++ src/server.cpp
@@ -4,16 +4,20 @@
 #include <iostream>
 Server* server;
 
+//função que finaliza o servidor
 void kill(int) {
     server->kill();
+    delete server;
+    exit(0);
 }
 
 int main() {
     server = new Server("cli-tcp-chat-server.conf");
+
+    //cadastra os sinais para garantir que o servidor finalize corretamente
     std::signal(SIGINT, kill);
     std::signal(SIGTERM, kill);
     std::signal(SIGHUP, kill);
     server->run();
-    delete server;
     return 0;
 }
