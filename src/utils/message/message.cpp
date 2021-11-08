ee547d8c8b374ad5589d221d60d67675c18dc9d1
Matheus Vieira <matheus.v4200@gmail.com>
2021-11-08 16:32:30 -0300

    documentaçao do message

--- src/utils/message/message.cpp
+++ src/utils/message/message.cpp
@@ -1,6 +1,7 @@
 #include "message.hpp"
 
 Message::Message(std::string sender, std::string content) {
+    //pega a data atual
     std::time_t now = time(0);
 
     struct tm* timeinfo;
@@ -9,17 +10,21 @@ Message::Message(std::string sender, std::string content) {
     time(&now);
     timeinfo = localtime(&now);
 
+    //formata
     strftime(buffer, 80, "%d/%m/%y %I:%M%p", timeinfo);
 
+    //monta a mensagem
     data += "[" + std::string(buffer) + "]\n" + sender + ": " + content;
     size = data.length() + 1;
 }
 
 Message::Message(std::string content) {
+    //pega a data atual
     std::time_t now = time(0);
     std::string date = std::ctime(&now);
     date.pop_back();
 
+    //monta a mensagem
     data += "[" + date + "]: " + content;
     size = data.length() + 1;
 }
--- src/utils/message/message.hpp
+++ src/utils/message/message.hpp
@@ -10,6 +10,16 @@ class Message {
     int size;
 
    public:
+    /**
+	 * Construtor que insere um remetente e uma data na mensagem
+	 * @param sender remetente da mensagem
+	 * @param content conteúdo da mensagem
+	 */
     Message(std::string sender, std::string content);
+
+    /**
+	 * Construtor que cria uma mensagem apenas com conteúdo e data
+	 * @param content conteúdo da mensagem
+	 */
     Message(std::string content);
 };
