2a3c6f8dcb865755c587da39d98aa56fba38e08a
Matheus Vieira <matheus.v4200@gmail.com>
2021-11-07 23:13:42 -0300

    simplificando o controler

--- src/utils/global-controler/global-controler.cpp
+++ src/utils/global-controler/global-controler.cpp
@@ -1,14 +1,7 @@
 #include "./global-controler.hpp"
 
-std::vector<Message> GlobalControler::messageHistory = std::vector<Message>{};
-std::vector<std::string> GlobalControler::allChats = std::vector<std::string>{};
-std::vector<std::string> GlobalControler::currUsers = std::vector<std::string>{};
+std::vector<std::string> GlobalControler::messageHistory = std::vector<std::string>{};
 
-void GlobalControler::addNewMessage(Message newMessage) {
+void GlobalControler::addNewMessage(std::string newMessage) {
     GlobalControler::messageHistory.push_back(newMessage);
-}
-
-void GlobalControler::updateCurrUsers(std::vector<std::string> newCurrUsers) {
-    GlobalControler::currUsers.clear();
-    GlobalControler::currUsers.insert(GlobalControler::currUsers.end(), newCurrUsers.begin(), newCurrUsers.end());
 }
--- src/utils/global-controler/global-controler.hpp
+++ src/utils/global-controler/global-controler.hpp
@@ -1,19 +1,13 @@
 #pragma once
 #include <string>
 #include <vector>
-#include "../message/message.hpp"
 
-#define PORT 3030
-#define MAX_MESSAGE_SIZE 80
-#define ADDRESS "127.0.0.1"
+#include "../message/message.hpp"
 
 class GlobalControler {
    public:
-    static std::vector<Message> messageHistory;
-    static std::vector<std::string> currUsers;
-    static std::vector<std::string> allChats;
+    static std::vector<std::string> messageHistory;
 
    public:
-    static void addNewMessage(Message newMessage);
-    static void updateCurrUsers(std::vector<std::string> newCurrUsers);
+    static void addNewMessage(std::string newMessage);
 };
