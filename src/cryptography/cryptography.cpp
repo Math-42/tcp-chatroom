a373dfdd225a029ec392e8c741a9274ef3b402ee
Thiago Vicentini <thiago-650@usp.br>
2021-11-07 21:56:44 -0300

    Updating class

--- src/cryptography/cryptography.cpp
+++ src/cryptography/cryptography.cpp
@@ -1,95 +1,115 @@
 #include "cryptography.hpp"
 
+Cryptography::Cryptography(){
+    this->privateKey = 0;
+}
 Cryptography::Cryptography(long long int privateKey){
-	this->privateKey = privateKey;
+    this->privateKey = privateKey;
 }
 
 Cryptography::~Cryptography(){}
 
-bool Cryptography::isPrime(int a){
-	for (int i = a-1; i > 1; i--)
-		if (a%i == 0)
-			return false;
-	return true;
+bool Cryptography::isPrime(long long int number){
+    int tmp = 2;
+	while(tmp < number)
+		if(!(number % tmp++))
+			break;
+
+	if(tmp == number)
+		return true;
+	return false;
 }
 
-int Cryptography::nearPrime(int a){
+long long int Cryptography::nearPrime(int prime){
 	do{
-		if(isPrime(a))
-            return a;
-		a++;
-	} while (true);
+		if(isPrime(prime))
+            return prime;
+		prime++;
+	}while(true);
 }
 
-bool Cryptography::__gcd(int a, int b){
-	if(a == 0){
+long long int Cryptography::producePrime(){
+    long long int prime = 0;
+	srand(time(0));
+	while (true){
+		prime = (rand() % 500);
+		if(isPrime(prime))
+		    break;
+	}
+	return prime;
+}
+
+bool Cryptography::__gcd(long long int a, long long int b){
+    if(a == 0){
 		if(b > 1)
 			return false;
 		else
 			return true;
 	 }
-    return __gcd(b % a, a); 
+    return __gcd(b%a, a);
 }
 
-int Cryptography::__xgcd(int a, int b, int & x, int & y){
-    if(b == 0){
-       x = 1;
-       y = 0;
-       return a;
-    }
-
-    int x1, y1, gcd = __xgcd(b, a % b, x1, y1);
-    x = y1;
-    y = x1 - (a / b)*y1;
-    return gcd;
+long long int Cryptography::createEKey(long long int phi){
+    long long int eKey;
+	while(true){
+		eKey = rand()%phi;
+		if(eKey >= 2 && __gcd(eKey, phi) == 1)
+			break;
+	}
+	return eKey;
 }
 
-int Cryptography::cdn(int c, int d, int n){
-    int value = 1;
-    while(d > 0){
-        value *= c;
-        value %= n;
-        d--;
-    }
-    return value;
-}
-
-int Cryptography::Coprime(int a, int b){
-	vector<int> coprimes;
-	for(int i = 2; i < b; i++)
-		if(__gcd(i, a) && __gcd(i, b))
-			coprimes.push_back(i);
-
-	return coprimes[rand()%coprimes.size()];
+long long int Cryptography::createDKey(long long int eKey, long long int phi){
+    
+    long long int dKey = phi/eKey;
+	while(true){
+		if(((dKey*eKey) % phi) == 1)
+			break;
+		else 
+			++dKey;
+	}
+	return dKey;
 }
 
 void Cryptography::CreateKey(){
-	int p = nearPrime(this->privateKey);
-	int q = nearPrime(p + 2);
-	this->n = p * q;
-	int phi = (p - 1)*(q - 1);
-	this->publicKey = Coprime(this->n, phi);
-	this->privateKey = CalculateDecryptionKey(this->publicKey, phi);
+
+    long long int p, q;
+    if(this->privateKey != 0){
+        p = producePrime();
+        q = producePrime();
+        while(p == q)
+            p = producePrime();
+    }else{
+        p = nearPrime(this->privateKey);
+	    q = nearPrime(p + 2);
+    }
+
+	this->n = p*q;
+	long long int phi = (p - 1)*(q - 1);
+	this->publicKey = createEKey(phi);
+	this->privateKey = createDKey(this->publicKey, phi);
 }
 
-int Cryptography::CalculateDecryptionKey(int publicKey, int phi){
-	int x, y;
-	__xgcd(publicKey, phi, x, y);
-	if(x >= 0)
-		return x;
-	else
-		return phi + x;
+long long int Cryptography::TransformMessage(long long int msg, long long int key, long long int n){
+    
+    long long int value = 1;
+    while(key > 0){
+        value *= msg;
+        value %= n;
+        key--;
+    }
+    return value;
 }
 
 string Cryptography::RSA(string message){
 	CreateKey();
 	for(auto&a : message)
-		a = cdn(a-97, this->publicKey, this->n);
+		a = TransformMessage(a-97, this->publicKey, this->n);
 	return message;
 }
 
 string Cryptography::_RSA(string message){
 	for (auto&a : message) 
-		a = 97 + cdn(a, this->privateKey, this->n);
+		a = 97 + TransformMessage(a, this->privateKey, this->n);
 	return message;
 }
--- src/cryptography/cryptography.hpp
+++ src/cryptography/cryptography.hpp
@@ -1,29 +1,31 @@
 #pragma once
 #include <vector>
+#include <iostream>
 #include <string>
 
 using namespace std;
 
 class Cryptography {
     public:
+        Cryptography();
         Cryptography(long long int privateKey);
         ~Cryptography();
 
-        bool isPrime(int a);
-        int nearPrime(int a);
-        bool __gcd(int a, int b);
-        int __xgcd(int a, int b, int & x, int & y);
-        int cdn(int c, int d, int n);
-        int Coprime(int a, int b);
+        bool isPrime(long long int a);
+        long long int nearPrime(int prime);
+        long long int producePrime();
+        bool __gcd(long long int a, long long int b);
+        long long int TransformMessage(long long int c, long long int d, long long int n);
+        long long int createEKey(long long int phi);
+        long long int createDKey(long long int eKey, long long int phi);
         void CreateKey();
-        int CalculateDecryptionKey(int publicKey, int phi);
 
         string RSA(string message);
         string _RSA(string message);
 
-        int publicKey;
-        int n;
+        long long int publicKey;
+        long long int n;
 
     private:
-        int privateKey;
+        long long int privateKey;
 };
