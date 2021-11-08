f882fcf1a417326a38d585ca332b8cba300a6ca8
Matheus Vieira <matheus.v4200@gmail.com>
2021-11-08 15:03:48 -0300

    adding signal

--- src/client.cpp
+++ src/client.cpp
@@ -1,15 +1,31 @@
-#include <iostream>
+#include <csignal>
 
 #include "communicator/communicator.hpp"
 #include "gui/window.hpp"
 
+Window* clientWindow;
+Communicator* communicator;
+
+void kill(int) {
+    communicator->disconnect();
+    delete communicator;
+    delete clientWindow;
+}
+
+void nothing(int) {}
+
 int main(int argc, char* argv[]) {
     std::string userName = (argc >= 2) ? argv[1] : "anom";
 
-    Communicator* communicator = Communicator::getInstance(4040, 256, "127.0.0.1", userName);
-    Window clientWindow(50, 50, 1);
+    communicator = Communicator::getInstance(4040, 256, "127.0.0.1", userName);
 
-    clientWindow.show();
+    clientWindow = new Window(50, 50, 1);
+    std::signal(SIGWINCH, nothing);
+    std::signal(SIGINT, kill);
+    std::signal(SIGTERM, kill);
+    std::signal(SIGHUP, kill);
+
+    clientWindow->show();
     communicator->run();
 
     return 0;
