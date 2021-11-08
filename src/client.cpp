7a5c9f1cdd7d3955f9d23f1a5604373d8d094dd0
Matheus Vieira <matheus.v4200@gmail.com>
2021-11-07 23:13:15 -0300

    adicionando opção de inserir o username

--- src/client.cpp
+++ src/client.cpp
@@ -1,10 +1,16 @@
+#include <iostream>
+
 #include "communicator/communicator.hpp"
 #include "gui/window.hpp"
 
-int main() {
-    Communicator* communicator = Communicator::getInstance(3050, 80, "127.0.0.1");
+int main(int argc, char* argv[]) {
+    std::string userName = (argc >= 2) ? argv[1] : "anom";
+
+    Communicator* communicator = Communicator::getInstance(4040, 256, "127.0.0.1", userName);
     Window clientWindow(50, 50, 1);
+
     clientWindow.show();
     communicator->run();
+
     return 0;
 }
